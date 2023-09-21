3c57e91645ef256589b2f5e56a0ef9ccfdb68d91
pm-ihes <pascal.misler@ihes.io>
2023-09-21 23:26:07 +0200

    Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-test-bc9b9',
+    appId: '1:48560271025:web:8f6e9d5cca409e233bfc90',
+    storageBucket: 'fir-test-bc9b9.appspot.com',
+    apiKey: 'AIzaSyCNAEl-xPzb8uW3QlANDTXxG0mI65RMkTE',
+    authDomain: 'fir-test-bc9b9.firebaseapp.com',
+    messagingSenderId: '48560271025',
+  },
+};
